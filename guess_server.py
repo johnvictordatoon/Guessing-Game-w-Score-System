16cb986ad4d655b1f5e24ef8e59b8b036b018a4f
John Victor Dato-on <143370209+johnvictordatoon@users.noreply.github.com>
2024-05-02 11:57:11 +0800

    added username and error checking to difficulty

--- guess_server.py
+++ guess_server.py
@@ -1,5 +1,5 @@
 import socket
-import random 
+import random
 
 host = "localhost"
 port = 7777
@@ -13,6 +13,10 @@ c. Hard (1-500)
 d. Exit
 """
 
+def get_username(conn):
+    username = conn.recv(1024).decode().strip()
+    return username
+
 def generate_random_int(difficulty):
     if difficulty == "a":
         return random.randint(1, 50)
@@ -21,18 +25,24 @@ def generate_random_int(difficulty):
     elif difficulty == "c":
         return random.randint(1, 500)
     else:
-        raise ValueError("Invalid difficulty level")
+        return None
 
 # initialize the socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
 s.listen(5)
 
-print(f"Server Port {port}")
+print(f"Server Port: {port}")
+print("Waiting for connection...")
 
 while True:
     conn, addr = s.accept()
-    print(f"New User: {addr[0]}")
+
+    # Get username from the client
+    username = get_username(conn)
+    print(f"{username} is in the server.")
+
+    print(f"User address: {addr[0]}")
     conn.sendall(banner.encode())
 
     while True:
@@ -40,22 +50,24 @@ while True:
         difficulty = conn.recv(1024).decode().strip().lower()
 
         if difficulty == "d":
-            print("(user) left the server.")
+            print(f"{username} left the server.")
             conn.close()
             break
 
-        # generate random number based on difficulty
         guessme = generate_random_int(difficulty)
+        if guessme is None:
+            break
         print(f"Generated number to guess: {guessme}")
 
+        # Guessing loop
         while True:
             client_input = conn.recv(1024)
             guess = int(client_input.decode().strip())
-            print(f"User guess attempt: {guess}")
+            print(f"{username} guess attempt: {guess}")
             if guess == guessme:
                 conn.sendall(b"Correct Answer!")
                 break
             elif guess > guessme:
-                conn.sendall(b"\nGuess Lower!\n")
+                conn.sendall(b"\nGuess Lower!")
             else:
-                conn.sendall(b"\nGuess Higher!\n")
+                conn.sendall(b"\nGuess Higher!")
